0872716c39f98112ab6a5c0e41c95469c8f4fcbb
Oscar Chen <oscar1149412@gmail.com>
2026-03-06 12:22:55 -0800

    finish zip

--- part2/zip.py
+++ part2/zip.py
@@ -1,11 +1,17 @@
 from typing import List, Dict
 
 
+# that takes two lists, names and scores, and returns a dictionary where the key is names[i] and it maps to scores[i] as the value.
 def group_names_and_scores(names: List[str], scores: List[int]) -> Dict[str, int]:
-    pass
+    rtn = {}
+    for name, score in zip(names, scores):
+        rtn[name] = score
+    return rtn
 
 
 # do not modify below this line
 print(group_names_and_scores(["Alice", "Bob", "Charlie"], [90, 80, 70]))
 print(group_names_and_scores(["Jane", "Carol", "Charlie"], [25, 100, 60]))
 print(group_names_and_scores(["Doug", "Bob", "Tommy"], [80, 90, 100]))
+
+# zip creation and space is O(1), set at the beginning of the input lists
