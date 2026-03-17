a4c920f01e958e69bff0a3dc0f471efe3f0c83f8
Oscar Chen <oscar1149412@gmail.com>
2026-03-17 18:46:56 -0400

    max area of island

--- algos_basic/max_area_of_island.py
+++ algos_basic/max_area_of_island.py
@@ -1,6 +1,6 @@
 from typing import List
 from test_runner import assert_equal, run_tests
-
+from collections import deque
 '''
 695. Max Area of Island
 
@@ -37,7 +37,36 @@ class Solution:
         :param grid: 2D matrix of 0/1.
         :return: Maximum island area, or 0 if none.
         """
-        pass
+        max_size = 0
+        row_len, col_len = len(grid), len(grid[0])
+        
+        directions = [(1,0), (0, 1), (-1, 0), (0, -1)]
+
+        def bfs(grid, row, col):
+            queue = deque()
+            grid[row][col] = 0
+            queue.append((row, col))
+            area = 1
+            
+            while queue:
+                row, col = queue.popleft()
+                for d in directions:
+                    new_row = row + d[0]
+                    new_col = col + d[1]
+                    if not (0 <= new_row < row_len) or not (0 <= new_col < col_len) or grid[new_row][new_col] != 1:
+                        continue
+                    else:
+                        area += 1
+                        grid[new_row][new_col] = 0
+                        queue.append((new_row, new_col))
+            return area
+            
+        for i in range(row_len):
+            for j in range(col_len):
+                if grid[i][j] == 1:
+                    area = bfs(grid, i, j)
+                    max_size = max(area, max_size)
+        return max_size
 
 
 # ========== TESTS ==========
