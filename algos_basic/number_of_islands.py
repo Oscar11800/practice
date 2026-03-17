792a4c2a81829b872941bed3aa4e90f041ead03a
Oscar Chen <oscar1149412@gmail.com>
2026-03-17 18:12:44 -0400

    number of islands

--- algos_basic/number_of_islands.py
+++ algos_basic/number_of_islands.py
@@ -41,7 +41,25 @@ class Solution:
         :param grid: 2D grid of '0' and '1'
         :return: Number of islands
         """
-        pass
+        counter = 2
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        row_len, col_len = len(grid), len(grid[0])
+
+        def dfs(grid, row, col, counter):
+            if 0 <= row < row_len and 0 <= col < col_len and grid[row][col] == "1":
+                grid[row][col] = counter
+            else:
+                return
+            for d in directions:
+                dfs(grid, row + d[0], col + d[1], counter)
+        
+        
+        for i in range(row_len):
+            for j in range(col_len):
+                if grid[i][j] == "1":
+                    dfs(grid, i, j, counter)
+                    counter += 1
+        return counter - 2
 
 
 # ========== TESTS ==========
