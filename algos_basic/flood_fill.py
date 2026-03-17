96943553aa05cf7894d0f5be171bd63ea9590924
Oscar Chen <oscar1149412@gmail.com>
2026-03-17 15:21:26 -0400

    flood fill

--- algos_basic/flood_fill.py
+++ algos_basic/flood_fill.py
@@ -49,7 +49,22 @@ class Solution:
         :param color: Replacement color.
         :return: Modified image.
         """
-        pass
+        og_color = image[sr][sc]
+        if color == og_color:
+            return image
+        row_length = len(image)
+        col_length = len(image[0])
+        def dfs(r, c):
+            if r < 0 or r >= row_length or c < 0 or c >= col_length or image[r][c] != og_color:
+                return image
+            image[r][c] = color
+            dfs(r + 1, c)
+            dfs(r, c + 1)
+            dfs(r - 1, c)
+            dfs(r, c - 1)
+            
+        dfs(sr, sc)
+        return image
 
 
 # ========== TESTS ==========
