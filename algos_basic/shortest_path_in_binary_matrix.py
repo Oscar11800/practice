5eeb57f7a53c8d611bee5ed5427df4d4dcc7de66
Oscar Chen <oscar1149412@gmail.com>
2026-03-17 23:31:08 -0400

    shortest path in bin matrix

--- algos_basic/shortest_path_in_binary_matrix.py
+++ algos_basic/shortest_path_in_binary_matrix.py
@@ -1,6 +1,6 @@
 from typing import List
 from test_runner import assert_equal, run_tests
-
+from collections import deque
 '''
 1091. Shortest Path in Binary Matrix
 
@@ -41,7 +41,31 @@ class Solution:
         :param grid: n x n matrix of 0/1
         :return: shortest path length, or -1 if impossible
         """
-        pass
+        if grid[0][0] == 1 or grid[-1][-1] == 1:
+            return -1
+        row_len, col_len = len(grid), len(grid[0])
+        queue = deque()
+        visited = set()
+        queue.append((0,0))
+        visited.add((0, 0))
+        length = 1
+        directions = [(1,0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+
+        while queue:
+            for _ in range(len(queue)):
+                row, col = queue.popleft()
+                if row == row_len - 1 and col == col_len - 1:
+                    return length
+                for direction in directions:
+                    new_row, new_col = row + direction[0], col + direction[1]
+                    if not 0 <= new_row < row_len or not 0 <= new_col < col_len or grid[new_row][new_col] != 0 or (new_row, new_col) in visited:
+                        continue
+                    visited.add((new_row, new_col))
+                    queue.append((new_row, new_col))
+            length += 1
+
+        
+        return -1
 
 
 # ========== TESTS ==========
