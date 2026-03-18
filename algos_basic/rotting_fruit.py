3b631eaed1009f598e781a7da273085308688c81
Oscar Chen <oscar1149412@gmail.com>
2026-03-18 00:57:56 -0400

    rotting fruit

--- algos_basic/rotting_fruit.py
+++ algos_basic/rotting_fruit.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import List
 from test_runner import assert_equal, run_tests
 
@@ -35,7 +36,37 @@ class Solution:
         :param grid: 2D matrix of 0/1/2.
         :return: Minutes needed, or -1.
         """
-        pass
+        queue = deque()
+        fresh_count = 0
+        minutes = 0
+        row_len, col_len = len(grid), len(grid[0])
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        # initial fresh fruit count, setting rotten to queue
+        for i in range(row_len):
+            for j in range(col_len):
+                if grid[i][j] == 1:
+                    fresh_count += 1
+                elif grid[i][j] == 2:
+                    queue.append((i, j))
+        # edge case no fresh fruit
+        if fresh_count == 0:
+            return 0
+        
+        while queue:
+            for _ in range(len(queue)):
+                row, col = queue.popleft()
+                for d in directions:
+                    new_row, new_col = row + d[0], col + d[1]
+                    if not 0 <= new_row < row_len or not 0 <= new_col < col_len or not grid[new_row][new_col] == 1:
+                        continue
+                    queue.append((new_row, new_col))
+                    grid[new_row][new_col] = 2
+                    fresh_count -= 1
+            minutes += 1
+            if fresh_count == 0:
+                return minutes
+
+        return -1
 
 
 # ========== TESTS ==========
