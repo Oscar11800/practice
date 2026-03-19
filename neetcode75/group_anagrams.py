99b424ec06c1029abb84848eb2e7c24fed1b5fc7
Oscar Chen <oscar1149412@gmail.com>
2026-03-19 14:22:32 -0400

    group anagrams

--- neetcode75/group_anagrams.py
+++ neetcode75/group_anagrams.py
@@ -37,7 +37,21 @@ class Solution:
         :param strs: List of input strings.
         :return: List of anagram groups (order not important).
         """
-        pass
+        rtn = []
+        base = ord('a')
+        matchmap = {}
+
+        for s in strs:
+            charmap = {i : 0 for i in range(26)}
+            for c in s:
+                charmap[ord(c) - base] += 1
+            key = tuple(charmap.values())
+            if key in matchmap:
+                rtn[matchmap[key]].append(s)
+            else:
+                matchmap[key] = len(rtn)
+                rtn.append([s])
+        return rtn
 
 
 # ========== HELPERS ==========
