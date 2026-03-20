fdf93c37624ea63d8f2db9740d091903c8e970c6
Oscar Chen <oscar1149412@gmail.com>
2026-03-20 13:20:40 -0500

    container with most water

--- neetcode75/two_pointers/container_with_most_water.py
+++ neetcode75/two_pointers/container_with_most_water.py
@@ -29,7 +29,19 @@ Constraints:
 
 class Solution:
     def maxArea(self, heights: List[int]) -> int:
-        raise NotImplementedError
+        max_area = 0
+        l, r = 0, len(heights)-1
+        while l < r:
+            area = (r-l) * (min(heights[l], heights[r]))
+            max_area = max(area, max_area)
+            if heights[l] < heights[r]:
+                l += 1
+            elif heights[l] == heights[r]:
+                l += 1
+                r -= 1
+            else:
+                r -= 1
+        return max_area
 
 
 def test_example1():
