48461b9ab0fdbc093b855790ece0b49a8e2b814a
Oscar Chen <oscar1149412@gmail.com>
2026-03-20 14:09:10 -0500

    find min in sorted array

--- neetcode75/binary_search/find_minimum_in_rotated_sorted_array.py
+++ neetcode75/binary_search/find_minimum_in_rotated_sorted_array.py
@@ -40,7 +40,23 @@ Constraints:
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        raise NotImplementedError
+        minimum = nums[0]
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            minimum = min(minimum, nums[mid])
+            if nums[left] <= nums[mid]:
+                if nums[left] < nums[right]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[right] > nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return minimum
+
 
 
 def test_example1():
