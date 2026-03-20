d74ee618975a4ca31b2e026d50e145e5d9f5819a
Oscar Chen <oscar1149412@gmail.com>
2026-03-19 22:22:11 -0500

    fixed products of array

--- neetcode75/products_of_array_except_self.py
+++ neetcode75/products_of_array_except_self.py
@@ -41,11 +41,9 @@ class Solution:
         suffix = [1 for _ in range(n)]
         for i in range(1, n):
             prefix[i] = prefix[i-1] * nums[i-1]
-        print(prefix)
-        for i in range(n-2, 0, -1):
+        for i in range(n-2, -1, -1):
             suffix[i] = suffix[i+1] * nums[i+1]
-        print(suffix)
-        rtn = [i*j for i in prefix for j in suffix]
+        rtn = [p * s for p, s in zip(prefix, suffix)]
         return rtn
 
 
