47a4085d7fb58ba5a247b3ec23dce6440b25baa6
Oscar Chen <oscar1149412@gmail.com>
2026-03-20 08:40:42 -0500

    valid palindrome

--- neetcode75/two_pointers/valid_palindrome.py
+++ neetcode75/two_pointers/valid_palindrome.py
@@ -33,7 +33,17 @@ s is made up of only printable ASCII characters.
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        raise NotImplementedError
+        left, right = 0, len(s)-1
+        while left < right:
+            while not s[left].isalnum() and left < right:
+                left += 1
+            while not s[right].isalnum() and right > left:
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
 
 
 def test_example1():
