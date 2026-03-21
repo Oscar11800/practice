3de76c962a5ed0db782116753fc07aa916d9e695
Oscar Chen <oscar1149412@gmail.com>
2026-03-21 11:34:54 -0500

    same bin tree

--- neetcode75/trees/same_binary_tree.py
+++ neetcode75/trees/same_binary_tree.py
@@ -38,7 +38,33 @@ class TreeNode:
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        raise NotImplementedError
+        if not p and not q:
+            return True
+        if p and not q:
+            return False
+        if q and not p:
+            return False
+
+        stack = [(p, q)]
+        while stack:
+            p_node, q_node = stack.pop()
+            if p_node.val != q_node.val:
+                return False
+            if p_node.left:
+                if not q_node.left:
+                    return False
+                stack.append((p_node.left, q_node.left))
+            else:
+                if q_node.left:
+                    return False
+            if p_node.right:
+                if not q_node.right:
+                    return False
+                stack.append((p_node.right, q_node.right))
+            else:
+                if q_node.right:
+                    return False
+        return True
 
 
 def build_tree(vals):
