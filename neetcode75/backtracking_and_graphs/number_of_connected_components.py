a9724187a198d909bb06d0cc98188d42d48f4d06
Oscar Chen <oscar1149412@gmail.com>
2026-03-22 15:46:10 -0500

    number of conn components

--- neetcode75/backtracking_and_graphs/number_of_connected_components.py
+++ neetcode75/backtracking_and_graphs/number_of_connected_components.py
@@ -8,8 +8,7 @@ Medium
 You have a graph of n nodes. You are given an integer n and an array edges
 where edges[i] = [ai, bi] indicates that there is an edge between ai and bi
 in the graph.
-
-Return the number of connected components in the graph.
+self.Return the number of connected components in the graph.
 
 Example 1:
 Input: n = 5, edges = [[0,1],[1,2],[3,4]]
@@ -31,8 +30,33 @@ There are no repeated edges
 
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        raise NotImplementedError
+        parents = [i for i in range(n)]
+        ranks = [0 for _ in range(n)]
 
+        def find(node):
+            while parents[node] != node:
+                parents[node] = parents[parents[node]]
+                node = parents[node]
+            return parents[node]
+        def union(n1, n2):
+            node1, node2 = find(n1), find(n2)
+            if node1 == node2:
+                return False
+            rank1, rank2 = ranks[node1], ranks[node2]
+            if rank1 <= rank2:
+                parents[node1] = node2
+            elif rank1 > rank2:
+                ranks[node2] += 1
+            else:
+                parents[node1] = node2
+                ranks[node1] += 1
+            return True
+        unions = 0
+        for edge in edges:
+            a, b = edge
+            if union(a, b):
+                unions += 1
+        return n - unions
 
 def test_example1():
     sol = Solution()
