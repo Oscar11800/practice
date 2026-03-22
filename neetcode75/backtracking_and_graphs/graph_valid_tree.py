e02e0e5e40d96fb596394a579a3e855528146291
Oscar Chen <oscar1149412@gmail.com>
2026-03-22 14:05:35 -0500

    graph valid tree

--- neetcode75/backtracking_and_graphs/graph_valid_tree.py
+++ neetcode75/backtracking_and_graphs/graph_valid_tree.py
@@ -29,7 +29,22 @@ Constraints:
 
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        raise NotImplementedError
+        if len(edges) != n-1:
+            return False
+        # construct adj list
+        adj = [[] for _ in range(n)]
+        for edge in edges:
+            adj[edge[0]].append(edge[1])
+            adj[edge[1]].append(edge[0])
+        # dfs
+        self.visited = set()
+        def dfs(node):
+            self.visited.add(node)
+            for neighbor in adj[node]:
+                if neighbor not in self.visited:
+                    dfs(neighbor)
+        dfs(0)
+        return len(self.visited) == n
 
 
 def test_example1():
