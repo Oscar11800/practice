652ac304226f1d71e1cf4b6beddf45ffddb60934
Oscar Chen <oscar1149412@gmail.com>
2026-03-22 19:04:08 -0500

    median data stream

--- neetcode75/heaps/find_median_from_data_stream.py
+++ neetcode75/heaps/find_median_from_data_stream.py
@@ -1,8 +1,7 @@
 from neetcode75.test_runner import assert_equal, run_tests
-
+import heapq
 """
 Find Median From Data Stream
-Hard
 
 The median is the middle value in a sorted list of integers. For lists of
 even length, there is no middle value, so the median is the mean of the two
@@ -44,13 +43,27 @@ structure.
 class MedianFinder:
 
     def __init__(self):
-        raise NotImplementedError
+        self.min_heap = []
+        self.max_heap = []
 
     def addNum(self, num: int) -> None:
-        raise NotImplementedError
+        heapq.heappush(self.max_heap, -num)
+        if self.min_heap and self.max_heap and -self.max_heap[0] >= self.min_heap[0]:
+                heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+        while len(self.max_heap) - len(self.min_heap) > 1:
+            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+            # balance the tree
+        while len(self.min_heap) - len(self.max_heap) > 1:
+            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
 
     def findMedian(self) -> float:
-        raise NotImplementedError
+        if len(self.max_heap) == len(self.min_heap):
+            return (-self.max_heap[0] + self.min_heap[0]) / 2
+        else:
+            if len(self.max_heap) > len(self.min_heap):
+                return -self.max_heap[0]
+            else:
+                return self.min_heap[0]
 
 
 def test_example1():
