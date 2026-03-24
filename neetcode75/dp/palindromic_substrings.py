5314eb885e8e85d2f337becdb72ee5df1f57a25e
Oscar Chen <oscar1149412@gmail.com>
2026-03-24 18:54:03 -0500

    palindromic substrings

--- neetcode75/dp/palindromic_substrings.py
+++ neetcode75/dp/palindromic_substrings.py
@@ -28,7 +28,23 @@ s consists of lowercase English letters.
 
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        raise NotImplementedError
+        rtn = 0
+        n = len(s)
+
+        for i in range(n):
+            left = right = i
+            while left >= 0 and right < n and s[left] == s[right]:
+                rtn += 1
+                left -= 1
+                right += 1
+            left = i
+            right = i+1
+            while left >= 0 and right < n and s[left] == s[right]:
+                rtn += 1
+                left -= 1
+                right += 1
+
+        return rtn
 
 
 def test_example1():
