989162e5e830e8c038249ec47ed262d81a76051b
Oscar Chen <oscar1149412@gmail.com>
2026-03-25 00:41:53 -0500

    decode ways

--- neetcode75/dp/decode_ways.py
+++ neetcode75/dp/decode_ways.py
@@ -2,7 +2,6 @@ from neetcode75.test_runner import assert_equal, run_tests
 
 """
 Decode Ways
-Medium
 
 A string consisting of uppercase english characters can be encoded to a number
 using the following mapping:
@@ -39,7 +38,19 @@ s consists of digits.
 
 class Solution:
     def numDecodings(self, s: str) -> int:
-        raise NotImplementedError
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[n] = 1
+        for i in range(n-1, -1, -1):
+            ways = 0
+            if 1 <= int(s[i]) <= 9:
+                ways += dp[i+1]
+            if i + 1 < n and 10 <= int(s[i:i+2]) <= 26:
+                ways += dp[i+2]
+            dp[i] = ways
+        return dp[0]
+
+
 
 
 def test_example1():
