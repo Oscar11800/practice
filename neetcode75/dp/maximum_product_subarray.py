0329f5b964d501301c16bb73f762e36c17201aa6
Oscar Chen <oscar1149412@gmail.com>
2026-03-25 14:08:38 -0500

    max prod subarray

--- neetcode75/dp/maximum_product_subarray.py
+++ neetcode75/dp/maximum_product_subarray.py
@@ -28,8 +28,16 @@ Constraints:
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        raise NotImplementedError
+        rtn = nums[0]
+        max_product = 1
+        min_product = 1
+        for i in range(len(nums)):
+            temp = max_product
+            max_product = max(nums[i], nums[i] * max_product, nums[i] * min_product)
+            min_product = min(nums[i], nums[i] * temp, nums[i] * min_product)
+            rtn = max(max_product, rtn)
 
+        return rtn
 
 def test_example1():
     sol = Solution()
