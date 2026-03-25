72843a1e83469d6a9f1cfd1d248a043962d2de93
Oscar Chen <oscar1149412@gmail.com>
2026-03-25 17:05:16 -0500

    max subarray

--- neetcode75/greedy/maximum_subarray.py
+++ neetcode75/greedy/maximum_subarray.py
@@ -26,7 +26,14 @@ Constraints:
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        raise NotImplementedError
+        rtn = float('-inf')
+        curr_sum = 0
+        for num in nums:
+            curr_sum += num
+            rtn = max(curr_sum, rtn)
+            if curr_sum < 0:
+                curr_sum = 0
+        return rtn
 
 
 def test_example1():
